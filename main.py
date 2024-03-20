44beaa7a4cf022bfdfa1bb09b4ba6c41d44f4b70
abej <106447057+XdIsCool@users.noreply.github.com>
2024-03-20 00:46:43 -0700

    formatting

--- main.py
+++ main.py
@@ -84,7 +84,6 @@ class Game:
         if self.player.hitpoints < 1:
             self.playing = False
         self.all_sprites.update()
-        # self.collide_with_group(self.game.mobs, True)
 
     def draw_grid(self):
         for x in range(0, WIDTH, TILE_SIZE):
